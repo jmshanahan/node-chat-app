1cc4433cd1c8a1bbeb923ce2141dfe2226defd0a
jmshanahan <comeraghsolutions@gmail.com>
2017-08-28 15:16:57 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom(){
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        console.log("Should scroll");
+        messages.scrollTop(scrollHeight);
+    }    
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -14,9 +31,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-    // var li = jQuery('<li></li>');
-    // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    // jQuery('#messages').append(li);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -27,8 +42,8 @@ socket.on('newLocationMessage', function(message){
         from: message.from,
         createdAt: formattedTime
     });
-
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
