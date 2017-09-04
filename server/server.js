d17a1d2f32c976579c03cebff2370ad1e9ab2b69
jmshanahan <comeraghsolutions@gmail.com>
2017-09-04 14:10:52 +0100

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
-const { generateMessage, generateLocationMessage } = require('./utils/message');
-const { isRealString } = require('./utils/validation');
-const {Users} = require('./utils/users');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+const { generateMessage, generateLocationMessage } = require("./utils/message");
+const { isRealString } = require("./utils/validation");
+const { Users } = require("./utils/users");
 
 var app = new express();
 var server = http.createServer(app);
@@ -13,46 +13,65 @@ const port = process.env.PORT || 3000;
 var io = socketIO(server);
 var users = new Users();
 
-io.on('connection', (socket) => {
-    console.log('New user connected');
-    socket.on('join', (params, callback) => {
-        if (!isRealString(params.name) || !isRealString(params.room)) {
-            return callback("Name and Room name are required.");
-        }
-        socket.join(params.room);
-        users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room);
-        io.to(params.room).emit('updateUserList',users.getUserList(params.room));
-        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+io.on("connection", socket => {
+  console.log("New user connected");
+  socket.on("join", (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      return callback("Name and Room name are required.");
+    }
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+    io.to(params.room).emit("updateUserList", users.getUserList(params.room));
+    socket.emit(
+      "newMessage",
+      generateMessage("Admin", "Welcome to the chat app")
+    );
 
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
-        callback();
+    socket.broadcast
+      .to(params.room)
+      .emit(
+        "newMessage",
+        generateMessage("Admin", `${params.name} has joined`)
+      );
+    callback();
+  });
+  socket.on("createMessage", (message, callback) => {
+    var user = users.getUser(socket.id);
 
-    });
-    socket.on('createMessage', (message, callback) => {
-        console.log("Create message ", message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
-        callback();
-    });
-    socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin',
-            coords.latitude, coords.longitude));
-    });
-    socket.on('disconnect', () => {
-        var user = users.removeUser(socket.id);
-        if(user){
-            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
-        }
+    if (user && isRealString(message.text)) {
+      io
+        .to(user.room)
+        .emit("newMessage", generateMessage(user.name, message.text));
+    }
 
-    });
+    callback();
+  });
+  socket.on("createLocationMessage", coords => {
+    var user = users.getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "newLocationMessage",
+        generateLocationMessage(user.name, coords.latitude, coords.longitude)
+      );
+    }
+  });
+  socket.on("disconnect", () => {
+    var user = users.removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+      io
+        .to(user.room)
+        .emit("newMessage", generateMessage("Admin", `${user.name} has left.`));
+    }
+  });
 });
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('/', (req, res) => {
-
-    res.render('index');
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-server.listen(port, () => { console.log(`Server is up on port ${port}`) });
-
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
